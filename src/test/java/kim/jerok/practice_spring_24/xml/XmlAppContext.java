916c112cb8f465c05ed3f6eb61ec6e1674ef0e0d
jerok <jerok.kim@gmail.com>
2023-05-10 18:44:40 +0900

    practice spring

---
+++ src/test/java/kim/jerok/practice_spring_24/xml/XmlAppContext.java
@@ -0,0 +1,18 @@
+package kim.jerok.practice_spring_24.xml;
+
+import kim.jerok.practice_spring_24.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+}
