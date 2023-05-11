4d472612af5c6d1d81b16a2fa20535479cac28d1
jerok <jerok.kim@gmail.com>
2023-05-11 19:12:56 +0900

    practice spring

---
+++ src/main/java/kim/jerok/practice_spring_24/web/LogDemoService.java
@@ -0,0 +1,17 @@
+package kim.jerok.practice_spring_24.web;
+
+import kim.jerok.practice_spring_24.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+
+}
